e6a7d53b2786485d91a35571cd4ae3caccccf6df
dieantu <whoami@dieantu.com>
2022-07-21 09:46:06 +0200

    Initial Commit

---
+++ SwiftUILab/SwiftUILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILabApp.swift
+//  SwiftUILab
+//
+//  Created by Antu Segura on 21/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
