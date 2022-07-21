68d02298c1a7794ea795d4638fbf7e331a71d333
dieantu <whoami@dieantu.com>
2022-07-21 13:37:01 +0200

    Add ButtonsUI

---
+++ SwiftUILab/Views/Components/ButtonsView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonsView.swift
+//  SwiftUILab
+//
+//  Created by Antu Segura on 21/7/22.
+//
+
+import SwiftUI
+
+struct ButtonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonsView()
+    }
+}
